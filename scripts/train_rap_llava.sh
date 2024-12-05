1a65d4e15dcd16d454625c5ea62f0f38e35dc6ea
Hoar012 <277133291@qq.com>
2024-12-06 00:56:03 +0800

    update scripts

--- scripts/train_rap_llava.sh
+++ scripts/train_rap_llava.sh
@@ -2,10 +2,10 @@
 
 deepspeed llava/train/train.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3.json \
-    --model_name_or_path MBZUAI/LLaVA-Phi-3-mini-4k-instruct \
-    --version phi3_instruct \
-    --data_path ./data \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path liuhaotian/llava-v1.5-13b \
+    --version v1 \
+    --data_path ./data/your_dataset \
     --image_folder ./data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
@@ -15,7 +15,7 @@ deepspeed llava/train/train.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-rap-phi3-mini-lora \
+    --output_dir ./checkpoints/rap-llava-v1.5-13b-lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
@@ -30,7 +30,7 @@ deepspeed llava/train/train.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 4096 \
+    --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
--- scripts/train_rap_phi3.sh
+++ scripts/train_rap_phi3.sh
@@ -2,9 +2,9 @@
 
 deepspeed llava/train/train.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero2.json \
-    --model_name_or_path liuhaotian/llava-v1.5-13b \
-    --version v1 \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path MBZUAI/LLaVA-Phi-3-mini-4k-instruct \
+    --version phi3_instruct \
     --data_path ./data/your_dataset \
     --image_folder ./data \
     --vision_tower openai/clip-vit-large-patch14-336 \
@@ -15,8 +15,8 @@ deepspeed llava/train/train.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-lora \
-    --num_train_epochs 3 \
+    --output_dir ./checkpoints/llava-v1.5-rap-phi3-mini-lora \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
@@ -30,7 +30,7 @@ deepspeed llava/train/train.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 4096 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
